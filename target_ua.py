ca6a5f4e290b7dcd714f0c116015a6dd6f1e3c19
nazar12314 <kononenkonazar47@gmail.com>
2021-11-02 11:08:01 +0200

    Finished last function and made a test of a game, test completed successfully

--- target_ua.py
+++ target_ua.py
@@ -4,7 +4,7 @@ import random
 def generate_grid():
     letters = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя'
     grid = []
-    for _ in range(5):
+    while len(grid) < 5:
         letter = random.choice(letters)
         if letter not in grid:
             grid.append(letter)
@@ -20,7 +20,7 @@ def get_words(f, letters):
             line = line.lower().replace('\n', '')
             items.append(line.split(' ')[:2])
     for i in items:
-        if i[0] and i[0][0] in letters and len(i[0]) <= 5 and\
+        if i[0] and i[0][0] in letters and len(i[0]) <= 5 and \
                 'intj' not in i[1] and 'noninfl' not in i[1] and 'onomat' not in i[1]:
             for j in language_form:
                 if j in i[1]:
@@ -30,11 +30,27 @@ def get_words(f, letters):
 
 
 def check_user_words(user_words, language_part, letters, dict_of_words):
-    pass
+    correct_words = []
+    pure_words = []
+    words = [i[0] for i in list(filter(lambda x: x[1] == language_part, dict_of_words))]
+    for i in user_words:
+        if i in words and i[0] in letters:
+            correct_words.append(i)
+    for i in words:
+        if i not in correct_words:
+            pure_words.append(i)
+    return correct_words, pure_words
 
 
 def main():
-    pass
+    language_form = random.choice(('verb', 'adverb', 'adjective', 'noun'))
+    generated_letters = generate_grid()
+    print(generated_letters)
+    print(f'Language part: {language_form}')
+    user_input = input('>>> ').split(' ')
+    words = get_words('base.txt', generated_letters)
+    print(check_user_words(user_input, language_form, generated_letters, words))
 
 
-print(get_words('base.txt', generate_grid()))
+if __name__ == '__main__':
+    main()
