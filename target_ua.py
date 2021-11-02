efec7f7d2fbc91729be29808ec137b3ba40007ed
nazar12314 <kononenkonazar47@gmail.com>
2021-11-02 10:27:16 +0200

    Ended get_words function

--- target_ua.py
+++ target_ua.py
@@ -13,20 +13,20 @@ def generate_grid():
 
 def get_words(f, letters):
     items = []
+    result = []
+    language_form = {'/n': 'noun', 'noun': 'noun', '/adj': 'adjective', 'adv': 'adverb', '/v': 'verb', 'verb': 'verb'}
     with open(f, 'r') as file:
         for line in file:
-            line = line.replace('\n', '')
+            line = line.lower().replace('\n', '')
             items.append(line.split(' ')[:2])
-    for i in range(len(items)):
-        if ('/n' or 'noun') in items[i][1]:
-            items[i][1] = 'noun'
-        elif '/adj' in items[i][1]:
-            items[i][1] = 'adjective'
-        elif '/v' in items[i][1]:
-            items[i][1] = 'verb'
-        elif 'adv' in items[i][1]:
-            items[i][1] = 'adverb'
-    return items
+    for i in items:
+        if i[0] and i[0][0] in letters and len(i[0]) <= 5 and\
+                'intj' not in i[1] and 'noninfl' not in i[1] and 'onomat' not in i[1]:
+            for j in language_form:
+                if j in i[1]:
+                    i[1] = language_form[j]
+            result.append(tuple(i))
+    return result
 
 
 def check_user_words(user_words, language_part, letters, dict_of_words):
@@ -36,4 +36,5 @@ def check_user_words(user_words, language_part, letters, dict_of_words):
 def main():
     pass
 
-print(get_words('base.txt', 12))
+
+print(get_words('base.txt', generate_grid()))
